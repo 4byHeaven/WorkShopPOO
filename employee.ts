0ab6a2fe916074291f3a704a9aa81a7533a5da15
4byHeaven <samuelaby.men1705@gmail.com>
2024-11-17 10:07:36 -0500

    punto numero 6

---
+++ employee.ts
@@ -0,0 +1,19 @@
+
+import { Person } from "./Person.ts";
+
+export class Employee extends Person {
+    salary: number;
+    constructor(nombre: string, edad: number, salary: number) {
+        super(nombre, edad);
+        this.salary = salary;
+    }
+
+
+    saludar(): void {
+        console.log(`Hola, mi nombre es ${this.name}, tu edad es ${this.getage()} y el salario es de ${this.salary}.`);
+    }
+
+    trabajar(hours: number): void {
+        console.log(`Trabaja ${hours} horas al día.`);
+    }
+}
