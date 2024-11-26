57ef5bc485a54b5a5d9c222086cfcd0faf2bb2dd
4byHeaven <samuelaby.men1705@gmail.com>
2024-11-26 15:20:47 -0500

    punto 12

---
+++ Json.ts
@@ -0,0 +1,113 @@
+[
+    {
+      "id": 1,
+      "name": "Mickey Mouse",
+      "age": 30,
+      "salary": 900,
+      "direccion": {
+        "calle": "Calle Principal 1",
+        "ciudad": "Ciudad Animada",
+        "pais": "Estados Unidos"
+      }
+    },
+    {
+      "id": 2,
+      "name": "Pato Donald",
+      "age": 25,
+      "salary": 850,
+      "direccion": {
+        "calle": "Avenida de los Patos 22",
+        "ciudad": "Villa Pato",
+        "pais": "Estados Unidos"
+      }
+    },
+    {
+      "id": 3,
+      "name": "Bugs Bunny",
+      "age": 28,
+      "salary": 870,
+      "direccion": {
+        "calle": "Calle de las Zanahorias 45",
+        "ciudad": "Alburquerque",
+        "pais": "Estados Unidos"
+      }
+    },
+    {
+      "id": 4,
+      "name": "Bob Esponja",
+      "age": 35,
+      "salary": 910,
+      "direccion": {
+        "calle": "Fondo de Bikini 12",
+        "ciudad": "Fondo de Bikini",
+        "pais": "Océano Pacífico"
+      }
+    },
+    {
+      "id": 5,
+      "name": "Homero Simpson",
+      "age": 39,
+      "salary": 930,
+      "direccion": {
+        "calle": "Avenida Siempreviva 742",
+        "ciudad": "Springfield",
+        "pais": "Estados Unidos"
+      }
+    },
+    {
+      "id": 6,
+      "name": "Scooby-Doo",
+      "age": 7,
+      "salary": 750,
+      "direccion": {
+        "calle": "Camino Misterioso 88",
+        "ciudad": "Coolsville",
+        "pais": "Estados Unidos"
+      }
+    },
+    {
+      "id": 7,
+      "name": "Pedro Picapiedra",
+      "age": 40,
+      "salary": 890,
+      "direccion": {
+        "calle": "Carretera de Piedra 123",
+        "ciudad": "Piedradura",
+        "pais": "Prehistoria"
+      }
+    },
+    {
+      "id": 8,
+      "name": "Dora la Exploradora",
+      "age": 8,
+      "salary": 780,
+      "direccion": {
+        "calle": "Camino Aventura 67",
+        "ciudad": "Exploradores",
+        "pais": "Peru"
+      }
+    },
+    {
+      "id": 9,
+      "name": "Tom Gato",
+      "age": 6,
+      "salary": 770,
+      "direccion": {
+        "calle": "Avenida Jerry 98",
+        "ciudad": "Ciudad Animada",
+        "pais": "Estados Unidos"
+      }
+    },
+    {
+      "id": 10,
+      "name": "Jerry Ratón",
+      "age": 5,
+      "salary": 780,
+      "direccion": {
+        "calle": "Calle del Queso 101",
+        "ciudad": "Ciudad Animada",
+        "pais": "Estados Unidos"
+      }
+    }
+  ]
+  
